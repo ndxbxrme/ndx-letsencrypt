40d38ed34724acd015b359f94ada6790876ca46e
ndxbxrme <lewis_the_cat@hotmail.com>
2017-02-14 03:42:08 -0800

    update

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@
   'use strict';
   module.exports = function(ndx) {
     return ndx.app.get('/.well-known/acme-challenge/:content', function(req, res) {
-      return res.send('xxxxxxxxxxxx-yyyy.zzzzzzzzzzzzzzzzzzz');
+      return res.send('wlK2-xW6qis9VN4IadbdiiONGhlGma1Kd4XrsWHtdGU.Y9ft-MrxCgc0zu5wbEv0B04Zj1PD9YjPsQBzNwQmOnQ');
     });
   };
 
--- src/index.coffee
+++ src/index.coffee
@@ -2,4 +2,4 @@
 
 module.exports = (ndx) ->
   ndx.app.get '/.well-known/acme-challenge/:content', (req, res) ->
-    res.send 'xxxxxxxxxxxx-yyyy.zzzzzzzzzzzzzzzzzzz'
+    res.send 'wlK2-xW6qis9VN4IadbdiiONGhlGma1Kd4XrsWHtdGU.Y9ft-MrxCgc0zu5wbEv0B04Zj1PD9YjPsQBzNwQmOnQ'
